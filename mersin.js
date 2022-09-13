83b1bb89f5116ea525a6c12f1df051edbd834de7
ozturkoglukagan <ozturkoglukagan@gmail.com>
2022-09-13 14:49:33 +0300

    mersin dosyalari eklendi

---
+++ mersin.js
@@ -0,0 +1,15 @@
+// Fonksiyon
+// alert
+// var 
+// log
+// console log;
+var anapara = 100;
+
+
+function mesaj(){
+    let isim = document.getElementById("isim").value
+    var para = document.getElementById("numara").value
+    para = anapara - para
+    alert(isim+ " senin " + para + " paran kaldi.")
+
+}
