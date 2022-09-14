b1efc65558e547a5c75a70464e90befc7ee6e3fc
ozturkoglukagan <ozturkoglukagan@gmail.com>
2022-09-14 14:51:26 +0300

    izmir dosyalari eklendi

---
+++ izmirjs.js
@@ -0,0 +1,15 @@
+// var x = 100
+// let y = 50
+// const
+// alert
+// console.log();
+// function name(params) {
+    
+// }
+
+
+function mesaj() {
+    var nickname = document.getElementById("isim").value
+    let varlik = document.getElementById("sayi").value
+    alert(nickname+" 'in " +    varlik+ " tl parasi var.")
+}
