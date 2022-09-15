4990c5e9d538ef110f45ef9fe567b82b7c7783f8
ozturkoglukagan <ozturkoglukagan@gmail.com>
2022-09-15 15:11:22 +0300

    ankara dosyalari eklendi

---
+++ ankara.js
@@ -0,0 +1,14 @@
+// alert
+// var
+// let
+// const
+// console.log();
+// function
+
+// alert("siteme hosgeldiniz")
+
+function mesaj() {
+    var isim = document.getElementById("metin").value
+    let para = document.getElementById("sayi").value
+    alert( isim + " 'in "+para+" kadar parasi var.")
+}
